0272d16416f21d794b930acac65e449561373169
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-02-21 11:20:16 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+
+	_putchar('\n');
+}
