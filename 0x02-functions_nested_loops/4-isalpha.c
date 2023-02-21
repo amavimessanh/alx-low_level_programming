cf0bee50a62daa8f37be764bb0d9aac94845731f
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-02-21 13:15:06 -0800

    4-isalphafinal

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  *  _isalpha - checks for lowercase character
  * @c: is the char to be checked
  *
@@ -10,8 +9,8 @@
 
 int _isalpha(int c)
 {
-        if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
-                return (1);
-        else
-                return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
