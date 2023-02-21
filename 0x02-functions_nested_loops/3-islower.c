6d0ed645ca2520ab85de71a32e29dd05bfdb4762
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-02-21 12:50:40 -0800

    3-islowerfl

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,9 @@
 /**
  * _islower - checks for lowercase character
  * @c: is the char to be checked
- * Return 1 if c is lowercase  otherwise 0
+ *
+ * return 1 if c is lowercase
+ * return 0 otherwise
  *
  */
 
