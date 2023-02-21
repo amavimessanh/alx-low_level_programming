f698ce39a6092df2d429cda16456b4460c007b2d
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-02-21 12:46:11 -0800

    3-islowerfinal

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character
- * c: is the char to be checked
- * Return 1 if c is lowercase
- * Return 0 otherwise
+ * @c: is the char to be checked
+ * Return 1 if c is lowercase  otherwise 0
  *
  */
 
