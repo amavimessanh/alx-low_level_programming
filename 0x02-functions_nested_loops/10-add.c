4ec5d978df4e80c331297cfe1d7bbd88837435c0
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-02-23 09:13:21 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: The result of the addition.
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
