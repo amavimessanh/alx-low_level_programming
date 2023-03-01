0ef8244389dc4ee60872d217896fd0fe2a124286
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-03-01 10:37:49 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_number - that prints an integer
+  * @n: integer params
+  *
+  * Return: 0
+ **/
+
+void print_number(int n)
+{
+	unsigned int k;
+
+	k = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -n;
+	}
+
+	if (k / 10 != 0)
+	{
+		print_number(k / 10);
+	}
+	_putchar((k % 10) + '0');
+}
