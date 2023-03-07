c4c7e04af4247643196208b76c01a7aef314d01f
Amavi Bienvenu MESSANH <messanhbin@gmail.com>
2023-03-07 09:53:36 -0800

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_diagsums- sum diagonal values
+ *
+ * @a: pointer
+ * @size: int
+ *
+ * Return: sum of two sides
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[(size + 1) * i];
+	}
+	for (j = 0; j < size; j++)
+	{
+		sum2 += a[(size - 1) * (j + 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
